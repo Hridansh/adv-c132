629f8870178323fffc9818f4d484873da0644053
Hridansh <73342299+Hridansh@users.noreply.github.com>
2021-07-30 13:48:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function gaming_html(){
+    window = window.location("gaming.html");
+}
+
+function gfuel_html(){
+    window = window.location("gfuel.html");
+}
+
+function lv_html(){
+    window = window.location("lv bag.html");
+}
+
+function human_html(){
+    window = window.location("human.html");
+}
+
+function bulb_html(){
+    window = window.location("bulb.html");
+}
